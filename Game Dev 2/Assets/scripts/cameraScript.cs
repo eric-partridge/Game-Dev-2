5dbe8b23c2a9e127ca8a86be08513d6075ca1e3b
Eric Partridge <partre@rpi.edu>
2019-02-05 15:33:14 -0500

    Auto stash before merge of "master" and "origin/master"
    merge

---
+++ Game Dev 2/Assets/scripts/cameraScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour {
+
+    public Transform player;
+    public Transform pivot;
+    public float camRotateSpeed = 10f;
+    public Vector3 offset;
+    public bool useOffset;
+
+    private Quaternion rotation;
+
+
+    // Use this for initialization
+    void Start () {
+        if (!useOffset)
+        {
+            offset = player.position - transform.position;
+        }
+        pivot.transform.position = player.position;
+        pivot.transform.parent = player;
+    }
+	 
+	// Update is called once per frame
+	void LateUpdate () {
+
+        float horizontal = Input.GetAxis("Horizontal") * camRotateSpeed;
+        player.Rotate(0, horizontal, 0);
+        pivot.Rotate(0, 0, 0);
+
+        float yAngle = player.eulerAngles.y;
+        float xAngle = pivot.eulerAngles.x;
+        rotation = Quaternion.Euler(xAngle, yAngle, 0f);
+
+        transform.position = player.position - (rotation * offset);
+        transform.LookAt(player);
+    }
+}
---
+++ Game Dev 2/Assets/scripts/playerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    public float speed;
+    public float sensitivity;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float leftStickX = Input.GetAxis("Horizontal");
+        bool gas = Input.GetButton("R2");
+        bool reverse = Input.GetButton("L2");
+        Vector3 force = new Vector3(0,0,0);
+        if (gas)
+        {
+            //force = new Vector3(speed * transform.forward, 0f, 0f);
+            rb.AddForce(transform.forward * speed, ForceMode.VelocityChange);
+        }
+        else if (reverse)
+        {
+            rb.AddForce(transform.forward * -speed, ForceMode.VelocityChange);
+        }
+
+        //Quaternion deltaRotation = Quaternion.AngleAxis(leftStickX*sensitivity*Time.deltaTime, transform.up);
+
+        //rb.rotation = deltaRotation;
+	}
+}
