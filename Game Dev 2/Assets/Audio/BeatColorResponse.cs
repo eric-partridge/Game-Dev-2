a5da92fa3768d7996739fa5dade11944ddd788b0
Unknown <castrj5@rpi.edu>
2019-02-11 21:04:07 -0500

    Added Beat Timer

---
+++ Game Dev 2/Assets/Audio/BeatColorResponse.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatColorResponse : MonoBehaviour {
+
+    public Color target;
+    private Renderer rend;
+    private Color myColor;
+    private Color current;
+    public float rate = 0.1f;
+
+    // Use this for initialization
+    void Start () {
+        rend = GetComponent<Renderer>();
+        current = rend.material.GetColor("_EmissionColor");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(BPM_Clock.trigger == true)
+        {
+            myColor = new Color(rate * target.r, rate * target.g, rate * target.b);
+            rend.material.SetColor("_EmissionColor", myColor);
+            print("YES");
+        }
+        else
+        {
+            myColor = new Color(rate * current.r, rate * current.g, rate * current.b);
+            rend.material.SetColor("_EmissionColor", myColor);
+        }
+	}
+}
---
+++ Game Dev 2/Assets/Scripts/Audio/ParaColor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParaColor : MonoBehaviour {
+
+    public int freqBand = 0;
+    public Color target;
+    private Renderer rend;
+    private Color myColor;
+    private Color current;
+
+    // Use this for initialization
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        current = rend.material. GetColor("_Color");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myColor = new Color((Mathf.Abs(chuck.intensity[freqBand] - 1) * current.r) + (chuck.intensity[freqBand] * target.r),
+                            (Mathf.Abs(chuck.intensity[freqBand] - 1) * current.g) + (chuck.intensity[freqBand] * target.g),
+                            (Mathf.Abs(chuck.intensity[freqBand] - 1) * current.b) + (chuck.intensity[freqBand] * target.b), target.a);
+        rend.material.SetColor("_Color", myColor);
+    }
+}
