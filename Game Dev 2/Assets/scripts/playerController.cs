b3a53b2d071b14842582ec128e4c6c5e9ab08c26
Eric Partridge <partre@rpi.edu>
2019-04-01 22:29:36 -0400

    forgot to save everything before pushing

--- Game Dev 2/Assets/scripts/playerController.cs
+++ Game Dev 2/Assets/scripts/playerController.cs
@@ -166,7 +166,7 @@ public class playerController : MonoBehaviour {
             //camReference.GetComponent<cameraScript>().rotateCamera(-5f);
         }
 
-        if((hitTime + .75f) <= Time.fixedTime && hitByEnemy)
+        if((hitTime + 0.75f) <= Time.fixedTime && hitByEnemy)
         {
             maxSpeed /= hitChange;
             hitByEnemy = false;
