a58315acb06fa778b405f78c77187e2a75d261ae
Unknown <castrj5@rpi.edu>
2019-04-08 18:44:19 -0400

    Trying to debug drifting

--- Game Dev 2/Assets/scripts/playerController.cs
+++ Game Dev 2/Assets/scripts/playerController.cs
@@ -18,7 +18,9 @@ public class playerController : MonoBehaviour {
     public float boostChange;
     public float hitChange;
     public bool grounded;
+    public Vector3 rayNormal;
     public int playerNum;
+    public GameObject otherPlayer;
 
     private bool drifting = false;
     Rigidbody rb;
@@ -31,7 +33,6 @@ public class playerController : MonoBehaviour {
     private float boostTime = -2f;
     private float hitTime = -2f;
     private string R2Button;
-    private string R1Button;
     private string L2Button;
     private string L1Button;
     private string Horizontal;
@@ -51,12 +52,12 @@ public class playerController : MonoBehaviour {
         rb = this.GetComponent<Rigidbody>();
         defaultSensitivity = sensitivity;
         R2Button = "R2 P" + playerNum.ToString();
-        R1Button = "R1 P" + playerNum.ToString();
         L2Button = "L2 P" + playerNum.ToString();
         L1Button = "L1 P" + playerNum.ToString();
         Horizontal = "Horizontal P" + playerNum.ToString();
         RSX = "RSX P" + playerNum.ToString();
         RSY = "RSY P" + playerNum.ToString();
+        Physics.IgnoreCollision(this.GetComponent<Collider>(), otherPlayer.GetComponent<Collider>());
         print("len: " + Input.GetJoystickNames().Length);
     }
 
@@ -70,11 +71,13 @@ public class playerController : MonoBehaviour {
         // print(Input.GetAxis("RSX"));
         //print(Input.GetAxis("RSY"));
         float leftStickX = Input.GetAxis(Horizontal);
-        bool gas = (Input.GetButton(R2Button) || Input.GetButton(R1Button));
+        bool gas = Input.GetButton(R2Button);
         bool brake = (Input.GetButton(L2Button) || Input.GetButton(L1Button));
         Vector3 force = new Vector3(0,0,0);
 
         print("is brake: " + brake);
+
+        drift_direction = 0;
 
 
         if (isGrounded())
@@ -104,6 +107,23 @@ public class playerController : MonoBehaviour {
                     rb.velocity = rb.velocity * deacceleration;
                     sensitivity = defaultSensitivity;
                 }
+            }
+
+            if (drifting && drift_direction == 1)
+            {
+                leftStickX = Input.GetAxis(Horizontal) + 0.6f;
+                transform.Rotate(0, leftStickX / 1.6f * sensitivity, 0);
+                tempQuatR = new Quaternion(leftRotate50.x, leftRotate50.y, leftRotate50.z * leftStickX / 1.6f, leftRotate50.w);
+                shipModel.transform.localRotation = Quaternion.RotateTowards(shipModel.transform.localRotation, tempQuatR, 3f);
+                print("Drifting RIGHT");
+            }
+            else if (drifting && drift_direction == -1)
+            {
+                leftStickX = Input.GetAxis(Horizontal) - 0.6f;
+                transform.Rotate(0, leftStickX / 1.6f * sensitivity, 0);
+                tempQuatL = new Quaternion(rightRotate50.x, rightRotate50.y, rightRotate50.z * -leftStickX / 1.6f, rightRotate50.w);
+                shipModel.transform.localRotation = Quaternion.RotateTowards(shipModel.transform.localRotation, tempQuatL, 3f);
+                print("Drifting LEFT");
             }
             //once done braking, reset sensitivity to default
             if (!brake)
@@ -175,21 +195,7 @@ public class playerController : MonoBehaviour {
             print("Not slowed");
         }
 
-        if (drifting && drift_direction == 1)
-        {
-            leftStickX = Input.GetAxis(Horizontal) + 0.6f;
-            transform.Rotate(0, leftStickX / 1.6f * sensitivity, 0);
-            tempQuatR = new Quaternion(leftRotate50.x, leftRotate50.y, leftRotate50.z * leftStickX / 1.6f, leftRotate50.w);
-            shipModel.transform.localRotation = Quaternion.RotateTowards(shipModel.transform.localRotation, tempQuatR, 3f);
-        }
-        else if (drifting && drift_direction == -1)
-        {
-            leftStickX = Input.GetAxis(Horizontal) - 0.6f;
-            transform.Rotate(0, leftStickX / 1.6f * sensitivity, 0);
-            tempQuatL = new Quaternion(rightRotate50.x, rightRotate50.y, rightRotate50.z * -leftStickX / 1.6f, rightRotate50.w);
-            shipModel.transform.localRotation = Quaternion.RotateTowards(shipModel.transform.localRotation, tempQuatL, 3f);
-        }
-        else if (leftStickX != 0)
+        if (leftStickX != 0 && drift_direction == 0)
         {
             transform.Rotate(0, leftStickX * sensitivity, 0);
             if (leftStickX > 0)
